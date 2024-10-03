7bc7bf1cda9caec69285f1e2f38fabcf1d9ca58e
Tjaž Saksida <tjaz.saksida@gmail.com>
2024-10-03 12:49:33 +0200

    Prova

--- WebApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1d9f97d30b83ad66ad803255b452b1c26d80fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03471e54babbb3b4e231fcc287ca862f66a3c5a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
