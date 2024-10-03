bcb20144c6970f5deaffb812e78aaccacf83121f
Tjaž Saksida <tjaz.saksida@gmail.com>
2024-10-03 13:53:59 +0200

    Pr

---
+++ WebApp/WebApp/Migrations/20241003114058_Prva.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Prva : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Uporabniki",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ime = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    geslo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    jeActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Uporabniki", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Uporabniki_ime",
+                table: "Uporabniki",
+                column: "ime",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Uporabniki");
+        }
+    }
+}
