b8a1df6ca9d6d519ecf92653160ca2693225a741
= <=>
2024-10-04 08:03:48 +0200

    Aplikacija

---
+++ Aplication/Aplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Aplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Aplication/Aplication/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Aplication
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
